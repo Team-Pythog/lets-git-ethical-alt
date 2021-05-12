ffc16f2f192a45aca30560ec8867c2e31c0cfe77
Alex Angelico <alex.angelico@live.com>
2021-05-11 17:37:27 -0700

    updated nav routes, api URLs, local storage access

--- components/navigation.js
+++ components/navigation.js
@@ -3,9 +3,9 @@ export default function Navigation({ logoutEvent }) {
     <header className="w-5/6 m-auto">
       <nav className="w-full border border-black">
         <ul className="flex flex-row p-3">
-          <li className="w-1/5 p-3 text-center border border-black"><a href="../pages/homepage">Profile</a></li>
-          <li className="w-1/5 p-3 text-center border border-black"><a href="../pages/dilemmas">Dilemmas</a></li>
-          <li className="w-1/5 p-3 text-center border border-black"><a href="../pages/about">About Us</a></li>
+          <li className="w-1/5 p-3 text-center border border-black"><a href="/">Profile</a></li>
+          <li className="w-1/5 p-3 text-center border border-black"><a href="../dilemmas">Dilemmas</a></li>
+          <li className="w-1/5 p-3 text-center border border-black"><a href="../about">About Us</a></li>
           <li className="w-1/5 p-3 text-center border border-black"><a href="/" onClick={logoutEvent}>Logout</a></li>
         </ul>
       </nav>
